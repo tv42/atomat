b7e7644eaba46e1d9ff0bdc337f555db65046fa2
Tommi Virtanen <tv@eagain.net>
2008-08-01 21:07:31 +0300

    Use pygments, change directive from "python" to "sourcecode".

--- atomat/rst2entry.py
+++ atomat/rst2entry.py
@@ -1,3 +1,5 @@
+from __future__ import with_statement
+
 import os
 import urlparse
 from docutils.core import publish_string
@@ -6,17 +8,17 @@ from xml.dom import minidom
 import xml.dom
 import email.Utils
 from atomat import atom, xhtmlutil, datatypes
+from pygments import lexers, formatters, highlight
 
 from docutils.parsers import rst
 from docutils import nodes
 from cStringIO import StringIO
-from twisted.python import htmlizer
 
-def python(name, arguments, options, content, lineno,
-           content_offset, block_text, state, state_machine):
+def sourcecode(name, arguments, options, content, lineno,
+               content_offset, block_text, state, state_machine):
     filename = options.get('filename', None)
     if filename is None:
-        inp = StringIO('\n'.join(content).encode('utf-8'))
+        code = u'\n'.join(content)
     else:
         source = state_machine.input_lines.source(
             lineno - state_machine.input_offset - 1)
@@ -24,13 +26,23 @@ def python(name, arguments, options, content, lineno,
         filename = os.path.normpath(os.path.join(source_dir, filename))
         filename = utils.relative_path(None, filename)
         state.document.settings.record_dependencies.add(filename)
-        inp = file(filename)
-    outp = StringIO()
-    htmlizer.filter(inp, outp, writer=htmlizer.SmallerHTMLWriter)
-    html = outp.getvalue()
+        with file(filename) as f:
+            code = f.read().decode('utf-8')
 
     if arguments:
-        title_text = arguments[0]
+        (syntax,) = arguments
+    else:
+        syntax = 'text'
+    lexer = lexers.get_lexer_by_name(syntax)
+    formatter = formatters.HtmlFormatter()
+    html = highlight(
+        code=code,
+        lexer=lexer,
+        formatter=formatter,
+        )
+
+    title_text = options.get('title')
+    if title_text:
         text_nodes, messages = state.inline_text(title_text, lineno)
         title = nodes.caption('', '# ', *text_nodes)
     else:
@@ -46,12 +58,13 @@ def python(name, arguments, options, content, lineno,
 
     return [fig] + messages
 
-python.arguments = (0, 1, True)
-python.options = dict(filename=rst.directives.path,
-                      )
-python.content = 1
+sourcecode.arguments = (0, 1, True)
+sourcecode.options = dict(filename=rst.directives.path,
+                          title=rst.directives.unchanged,
+                          )
+sourcecode.content = 1
 
-rst.directives.register_directive('python', python)
+rst.directives.register_directive('sourcecode', sourcecode)
 
 def comment(name, arguments, options, content, lineno,
             content_offset, block_text, state, state_machine):
